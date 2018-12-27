de2e1979af8e8c9cc6a9cf6f5b3835294f08fbbf
Nik Gil <ngilevskiy@gmail.com>
2018-12-27 01:16:00 +0000

    Revert invalid test

--- test_database.py
+++ test_database.py
@@ -5,7 +5,6 @@ from database import Database
 
 class DatabaseTest(unittest.TestCase):
     def setUp(self):
-
         self.Database = Database("testing_db")
         self.db = self.Database.db
         self.Database.start_connection()
@@ -24,9 +23,7 @@ class DatabaseTest(unittest.TestCase):
         self.assertEquals(self.Database.Question.select().count(), 1)
         self.Database.add_new_question("What is the meaning of life?")
         self.assertEquals(self.Database.Question.select().count(), 2)
-        self.assertRaises(peewee.IntegrityError,
-                          self.Database.add_new_question("How much wood could a woodchuck chuck if a woodchuck could "
-                                                         "chuck wood?"))
+        self.Database.add_new_question("How much wood could a woodchuck chuck if a woodchuck could chuck wood?")
         self.assertEquals(self.Database.Question.select().count(), 2)
 
     def tearDown(self):
