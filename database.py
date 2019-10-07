79d97a22011823de1a2e3c56945ce72509a47dd2
Sam Griff <slgriff@slgriff.com>
2019-10-06 23:38:15 -0400

    #6: Add peewee prefix

--- database.py
+++ database.py
@@ -169,9 +169,9 @@ class Database:
         """
 
         if company is None:
-            q = self.Question.select().order_by(fn.Random()).get()
+            q = self.Question.select().order_by(peewee.fn.Random()).get()
         else:
-            q = self.Question.select().where(Question.company == company).order_by(fn.Random()).get()
+            q = self.Question.select().where(Question.company == company).order_by(peewee.fn.Random()).get()
 
         if q.leetcode is not None:
             return q.id, q.company, q.body, q.leetcode
